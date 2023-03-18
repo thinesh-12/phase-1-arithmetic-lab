98039a3e2b8a0c4866939dd1ced6bd310ccc6252
THINESHKUMAR <thinesh12@gmal.com>
2023-03-18 19:33:57 -0400

    edit

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1;
+let num3 =25; 
+let num4 = 7;
+let mod = num3 % num4;
+
+let max =Math.max(2,4,6,10,14,18,20);
